ee4d75c48f396feb1c39be25734e56de5a4239cc
Prince Menard T. Flores <floresprincemenard519@gmail.com>
2026-04-29 17:40:53 +0800

    input: added input for both numbers. number one and two.

--- simple_calculator.py
+++ simple_calculator.py
@@ -1,1 +1,3 @@
 operation = input("Welcome! I'm a simple calculator.\n" + "-" * 50 + "\nEnter operation (+, -, *, /): ")
+first_number = float(input("Enter first number: "))
+second_number = float(input("Enter second number: "))
