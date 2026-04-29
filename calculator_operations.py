72d48af4478e387c6c43f54fa4e10f286ad43031
Prince Menard T. Flores <floresprincemenard519@gmail.com>
2026-04-29 17:49:12 +0800

    added class for calculator. it will contain operations that will be called on another file.

---
+++ calculator_operations.py
@@ -0,0 +1,6 @@
+class Calculator:
+    def __init__(self, first_number, second_number, operation):
+        self.first_number = first_number
+        self.second_number = second_number
+        self.operation = operation
+    
