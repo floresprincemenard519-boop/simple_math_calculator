40d252371e36c3163491710c6d424a6befc772b9
Prince Menard T. Flores <floresprincemenard519@gmail.com>
2026-04-29 22:31:57 +0800

    AI auto corrected code to using a and b as variables. | I changed the variables and changed the parameters so that no violations are commited. | Might add cube root and special exponent method.

--- premuim_calculator.py
+++ premuim_calculator.py
@@ -1,12 +1,11 @@
 from basic_calculator import Calculator
 
 class PremiumCalculator(BasicCalculator):
-    def __init__(self):
-        super().__init__()
+    def __init__(self, first_number, second_number, operation):
+        super().__init__(first_number, second_number, operation)
 
-    def power(self, a, b):
-        return a ** b
-    
-    def square_root(self, a):
-        return a ** 0.5
-    
+    def power(self, first_number, second_number):
+        return first_number ** second_number
+
+    def square_root(self, number):
+        return number ** 0.5
