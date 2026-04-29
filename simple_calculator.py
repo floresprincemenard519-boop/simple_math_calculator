5521021d5006904f6d2a8cbb64fca51cb01c97ce
Prince Menard T. Flores <floresprincemenard519@gmail.com>
2026-04-29 22:33:49 +0800

    Imported premium_calculator to the main program.

--- simple_calculator.py
+++ simple_calculator.py
@@ -1,3 +1,7 @@
+from premuim_calculator import PremiumCalculator
+
 operation = input("Welcome! I'm a simple calculator.\n" + "-" * 50 + "\nEnter operation (+, -, *, /): ")
 first_number = float(input("Enter first number: "))
 second_number = float(input("Enter second number: "))
+
+calc = PremiumCalculator(first_number, second_number, operation)
