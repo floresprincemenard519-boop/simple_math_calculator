ff828e4e3c80c6ff18d24a8102c34e9f78a25422
Prince Menard T. Flores <floresprincemenard519@gmail.com>
2026-04-30 21:35:57 +0800

    removed init in child class.

--- premium_calculator.py
+++ premium_calculator.py
@@ -1,11 +1,7 @@
 from basic_calculator import Calculator
 
 class PremiumCalculator(Calculator):
-    def __init__(self, first_number, second_number, operation):
-        self.first_number = first_number
-        self.second_number = second_number
-        self.operation = operation
-        
+
     def power(self):
         return self.first_number ** self.second_number
 
