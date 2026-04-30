a10d2572f5b4252923ab1a14bd33496f3a287d51
Prince Menard T. Flores <floresprincemenard519@gmail.com>
2026-04-30 18:53:52 +0800

    Reset start from the beginning

--- simple_calculator.py
+++ simple_calculator.py
@@ -1,36 +1,17 @@
 from premuim_calculator import PremiumCalculator
 
-# operation = input(f"""Welcome! I'm a simple calculator. 
-# { "-" * 50 }
-# Choose an operation:
-# 1. Addition (+)
-# 2. Subtraction (-)
-# 3. Multiplication (*)
-# 4. Division (/)
-# 5. Power (**)
-# 6. Square Root (sqrt)
-# 7. Remainder (%)
-# { "-" * 50 }
-# Enter your choice (1-7): """)
+def run_calculator():
+    print(f"""Welcome! I'm a simple calculator. 
+    { "-" * 50 }
+    Choose an operation:
+    1. Addition (+)
+    2. Subtraction (-)
+    3. Multiplication (*)
+    4. Division (/)
+    5. Power (**)
+    6. Root (sqrt)
+    7. Remainder (%)
+    { "-" * 50 }
+    Enter your choice (1-7): """)
 
-# first_number = float(input("Enter first number: "))
-# second_number = float(input("Enter second number: "))
-
-# calculator = PremiumCalculator(first_number, second_number, operation)
-
-# if operation == "1":
-#     print(f"The result is: {calculator.addition()}")
-# elif operation == "2":
-#     print(f"The result is: {calculator.subtraction()}")
-# elif operation == "3":
-#     print(f"The result is: {calculator.multiplication()}")
-# elif operation == "4":
-#     print(f"The result is: {calculator.division()}")
-# elif operation == "5":
-#     print(f"The result is: {calculator.power(first_number, second_number)}")
-# elif operation == "6":    
-#     print(f"The result is: {calculator.square_root(first_number)}")
-# elif operation == "7":
-#     print(f"The result is: {calculator.remainder(first_number, second_number)}")
-# else:
-#     print("Invalid operation!")
+    
