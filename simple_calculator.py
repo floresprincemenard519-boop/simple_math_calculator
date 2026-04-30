0e7dda39dfcca3c06daeb4c87ecc424f8e66faf0
Prince Menard T. Flores <floresprincemenard519@gmail.com>
2026-04-30 19:03:50 +0800

    Created while loop for operation choice.

--- simple_calculator.py
+++ simple_calculator.py
@@ -2,7 +2,7 @@ from premuim_calculator import PremiumCalculator
 
 def run_calculator():
     print(f"""Welcome! I'm a simple calculator. 
-    { "-" * 50 }
+{ "-" * 50 }
     Choose an operation:
     1. Addition (+)
     2. Subtraction (-)
@@ -11,7 +11,14 @@ def run_calculator():
     5. Power (**)
     6. Root (sqrt)
     7. Remainder (%)
-    { "-" * 50 }
-    Enter your choice (1-7): """)
+    8. Exit
+{ "-" * 50 }""")
 
-    
+    while True:
+        choice = input("Enter your choice (1-8): ")
+
+        if choice == "8":
+            print("Thank you for using me! Goodbye!")
+            break
+
+run_calculator()
