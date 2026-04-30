6627337608e56021975e003c535476da24ac6afe
Prince Menard T. Flores <floresprincemenard519@gmail.com>
2026-04-30 21:19:14 +0800

    found the problem and fixed it | i added parameters which i shoudnt have cause its already in (self).

--- premuim_calculator.py
+++ premuim_calculator.py
@@ -7,22 +7,22 @@ class PremiumCalculator(Calculator):
         self.operation = operation
         super().__init__(first_number, second_number, operation)
 
-    def power(self, first_number, second_number):
-        return first_number ** second_number
+    def power(self):
+        return self.first_number ** self.second_number
 
     def square_root(self, number):
         if number < 0:
             return "Error: Square root of a negative number is not defined."
         return number ** 0.5
     
-    def radical(self, first_number, second_number):
-        if second_number == 0:
+    def radical(self):
+        if self.second_number == 0:
             return "Error: Cannot take the root of degree zero."
-        elif first_number < 0 and second_number % 2 == 0:
+        elif self.first_number < 0 and self.second_number % 2 == 0:
             return "Error: Even root of a negative number is not defined."
-        return first_number ** (1/second_number)
+        return self.first_number ** (1/self.second_number)
     
-    def remainder(self, first_number, second_number):
-        if second_number == 0:
+    def remainder(self):
+        if self.second_number == 0:
             return "Error: Division by zero is not allowed."
-        return first_number % second_number
+        return self.first_number % self.second_number
